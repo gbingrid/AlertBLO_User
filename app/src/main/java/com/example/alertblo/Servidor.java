aab96b16fdfa4697700a8102dedfaf39c4b42de1
Ingridgb <ingrid_gabu99@outlook.com>
2026-06-05 20:18:24 +0200

    Quinto commit: al parecer si funciona todo.

--- app/src/main/java/com/example/alertblo/Servidor.java
+++ app/src/main/java/com/example/alertblo/Servidor.java
@@ -29,7 +29,7 @@ public class Servidor {
             String body = "ID_DISPOSITIU=" + URLEncoder.encode(idDispositiu, "UTF-8")
                     + "&TEXT_ALERTA="  + URLEncoder.encode(textAlerta,   "UTF-8")
                     + "&SILENCIO=" + silencio;
-            HttpURLConnection conn = obrir(MainActivity.IP_SERVIDOR + "/crear_alerta_silencio.php", "POST");
+            HttpURLConnection conn = obrir(MainActivity.IP_SERVIDOR + "/create_alerta_silencio.php", "POST");
             conn.setDoOutput(true);
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             conn.getOutputStream().write(body.getBytes("UTF-8"));
