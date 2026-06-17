eb957672dd3713363d7d18c61c3ecdecac488225
Ingridgb <ingrid_gabu99@outlook.com>
2026-06-17 23:41:01 +0200

    Configuración final de funcionalidad para emitir sonido de alarma aunque el teléfono esté en silencio

--- app/src/main/java/com/example/alertblo/MainActivity.java
+++ app/src/main/java/com/example/alertblo/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.alertblo;
 
 import android.Manifest;
 import android.app.NotificationManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -155,6 +161,39 @@ public class MainActivity extends AppCompatActivity {
         } else {
             notificador.notify(idAlerta, creadorNotificacion.build());
         }
+
+        // Forzar sonido de la alerta aún cuando el móvil esté en silencio
+        if("canal_alerta".equals(canal)){
+            try{
+                AudioManager audioManager = (AudioManager) contexto.getSystemService(Context.AUDIO_SERVICE);
+                if(audioManager != null){
+                    int volAlerta = audioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+                    if(volAlerta == 0){
+                        // Subir volumen a nivel intermedio
+                        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, 6, 0);
+                    }
+                }
+
+                // Obtener la ruta del sonido
+                Uri rutaSonido = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + contexto.getPackageName() + "/raw/sonidoalarma");
+
+                // Crear reproductor Ringtone
+                Ringtone ringtone = RingtoneManager.getRingtone(contexto, rutaSonido);
+                if(ringtone != null){
+                     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                         ringtone.setAudioAttributes(new AudioAttributes.Builder()
+                                 .setUsage(AudioAttributes.USAGE_ALARM)
+                                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                                 .build());
+                     }
+                     ringtone.play();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
 
